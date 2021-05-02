dab3ebb46afa95186bf1298236da0e9d99399a5d
AbdelrahmanSaied98 <AbdelrhmanSaied98>
2021-05-02 03:06:42 -0700

    Adding checkmark functionality at TodoViewController

--- MyTodo/TodoListViewController.swift
+++ MyTodo/TodoListViewController.swift
@@ -24,7 +24,17 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else
+        {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
 }
 
